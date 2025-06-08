d726f81a599206265a9a147dcacf3f4fa21dd9ef
Hayk Davtyan <46712946+hayk96@users.noreply.github.com>
2025-06-09 01:06:50 +0400

    fix(prometheus-metrics): Fix metric type of DNS request latency (#4)
    * fix(prometheus-metrics): Use Summary metric type instead of Gauge for DNS latency calculation
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    
    * chore(prometheus-metrics): Remove metric DNS requests total as Summary type already provides
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    
    * chore(helm-charts) Bump app and chart version
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    
    ---------
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>

--- src/dns/dns_server.py
+++ src/dns/dns_server.py
@@ -119,17 +119,11 @@ class DNSServer:
                         "lookup_time_ms": (exec_time * 1000).__round__(3)
                     })
 
-                    metrics.kubebridge_dns_requests.labels(
-                        app_name="dns",
-                        record_type=dns.QTYPE[request.q.qtype],
-                        record_name=request.q.qname,
-                        record_ip=reply.short()).inc()
-
                     metrics.kubebridge_dns_lookup_time_seconds.labels(
                         app_name="dns",
                         record_type=dns.QTYPE[request.q.qtype],
                         record_name=request.q.qname,
-                        record_ip=reply.short()).set(exec_time)
+                        record_ip=reply.short()).observe(exec_time)
 
         except KeyboardInterrupt:
             logger.error("Server shutting down...")
